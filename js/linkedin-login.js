4c4742eadc086ba31e401d9fd7073f6614d76b5b
Luis <jean-louis.darmon@epitech.eu>
2014-07-17 16:11:48 +0200

    Linkedin Login in progress

---
+++ js/linkedin-login.js
@@ -0,0 +1,74 @@
+var linkedinapi = {
+    authorize: function(options) {
+        var deferred = $.Deferred();
+
+        //Build the OAuth consent page URL
+        var authUrl = 'https://www.linkedin.com/uas/oauth2/authorization?' + $.param({
+            client_id: options.client_id,
+            redirect_uri: options.redirect_uri,
+            response_type: 'code'
+        });
+
+        //Open the OAuth consent page in the InAppBrowser
+        var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
+
+        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
+        //which sets the authorization code in the browser's title. However, we can't
+        //access the title of the InAppBrowser.
+        //
+        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
+        //authorization code will get set in the url. We can access the url in the
+        //loadstart and loadstop events. So if we bind the loadstart event, we can
+        //find the authorization code and close the InAppBrowser after the user
+        //has granted us access to their data.
+        $(authWindow).on('loadstart', function(e) {
+            var url = e.originalEvent.url;
+            var code = /\?code=(.+)$/.exec(url);
+            var error = /\?error=(.+)$/.exec(url);
+
+            if (code || error) {
+                //Always close the browser when match is found
+                authWindow.close();
+            }
+
+            if (code) {
+                //Exchange the authorization code for an access token
+                $.post('https://www.linkedin.com/uas/oauth2/accessToken', {
+                    code: code[1],
+                    client_id: options.client_id,
+                    client_secret: options.client_secret,
+                    redirect_uri: options.redirect_uri,
+                    grant_type: 'authorization_code'
+                }).done(function(data) {
+                    deferred.resolve(data);
+                }).fail(function(response) {
+                    deferred.reject(response.responseJSON);
+                });
+            } else if (error) {
+                //The user denied access to the app
+                deferred.reject({
+                    error: error[1]
+                });
+            }
+        });
+
+        return deferred.promise();
+    }
+};
+
+$(document).on('deviceready', function() {
+    var $loginButton = $('#login a');
+    var $loginStatus = $('#login p');
+
+    $loginButton.on('click', function() {
+        linkedinapi.authorize({
+            client_id: '77mmcb71lyvzps',
+            client_secret: 'bopWSZtXGkWWVxD5',
+            redirect_uri: 'http://localhost'
+        }).done(function(data) {
+            $loginStatus.html('Access Token: ' + data.access_token);
+        }).fail(function(data) {
+            $loginStatus.html(data.error);
+        });
+    });
+});
