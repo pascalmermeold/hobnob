b1407295174309140d207f6fac23c63a89a7325c
Jean-Louis Darmon <Luis@mbpdejeanlouis.home>
2014-07-21 22:58:47 +0200

    Test DB Storage on device

--- js/linkedin-login.js
+++ js/linkedin-login.js
@@ -1,4 +1,5 @@
 var access_token;
+var db;
 
 var linkedinapi = {
   authorize: function(options) {
@@ -60,6 +61,8 @@ $(document).on('deviceready', function() {
   var $loginStatus = $('#login p');
 
   document.addEventListener("deviceready", initRead, false);
+  db = window.openDatabase("aftrworkDb", "1.0", "AftrWork DB", 1000000);
+  db.transaction(populateDB, errorCB, successCB);
   $loginButton.on('click', function() {
     linkedinapi.authorize({
       client_id: '77mmcb71lyvzps',
@@ -73,6 +76,40 @@ $(document).on('deviceready', function() {
     });
   });
 });
+
+function populateDB(tx) {
+  tx.executeSql('DROP TABLE IF EXISTS DEMO');
+  tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+  tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+  tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+function queryDB(tx) {
+  tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+}
+
+// Query the success callback
+//
+function querySuccess(tx, results) {
+  console.log("Returned rows = " + results.rows.length);
+  // this will be true since it was a select statement and so rowsAffected was 0
+  if (!results.rowsAffected) {
+    console.log('No rows affected!');
+    return false;
+  }
+  // for an insert statement, this property will return the ID of the last inserted row
+  console.log("Last inserted row ID = " + results.insertId);
+}
+
+function errorCB(err) {
+  alert("Error processing SQL: "+err.code);
+}
+
+function successCB() {
+  alert("success!");
+  db.transaction(queryDB, errorCB);
+}
+
 
 function initWrite() {
   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFsWrite, fail);
