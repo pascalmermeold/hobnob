af2c06d84ab49dd54c32847debcb344f314c7c0d
Jean-Louis Darmon <Luis@pc10.home>
2014-07-22 18:03:26 +0200

    PushNotif+queryToken at start+get /hello

--- js/linkedin-login.js
+++ js/linkedin-login.js
@@ -1,5 +1,6 @@
 var access_token;
 var db;
+var pushNotification;
 
 var linkedinapi = {
   authorize: function(options) {
@@ -60,7 +61,14 @@ $(document).on('deviceready', function() {
   var $loginButton = $('#login a');
   var $loginStatus = $('#login p');
 
-  document.addEventListener("deviceready", initRead, false);
+  var $testButton = $('#test a');
+  var $testStatus = $('#test p');
+
+  $testButton.on('click', function() {
+    initPushNotif();
+  });
+
+  loginVerif();
   $loginButton.on('click', function() {
     linkedinapi.authorize({
       client_id: '77mmcb71lyvzps',
@@ -68,7 +76,6 @@ $(document).on('deviceready', function() {
     }).done(function(data) {
       $loginStatus.html(data.access_token);
       access_token = data.access_token;
-      // initWrite();
       initDb();
     }).fail(function(data) {
       $loginStatus.html(data.error);
@@ -76,8 +83,50 @@ $(document).on('deviceready', function() {
   });
 });
 
-// DB on local mobile
 
+function loginVerif() {
+  alert("logVerif");
+  db = window.openDatabase("aftrworkDb", "1.0", "AftrWork DB", 1000000);
+  db.transaction(queryTokenDB, errorCB);
+}
+function queryTokenDB(tx) {
+  alert("queryTokenDB");
+  tx.executeSql("SELECT value FROM OPTIONS", [], querySuccessInit, errorCB);
+}
+
+// PushNotification
+function initPushNotif() {
+  alert("INIT PUSHNOTIF");
+  pushNotification = window.plugins.pushNotification;
+  pushNotification.register(tokenHandler,errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
+}
+function onNotificationAPN (event) {
+  if ( event.alert ){
+    navigator.notification.alert(event.alert);
+  }
+
+  if ( event.sound ){
+    var snd = new Media(event.sound);
+    snd.play();
+  }
+
+  if ( event.badge ){
+    pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+  }
+}
+function successHandler (result) {
+    alert('result = ' + result);
+}
+function errorHandler (error) {
+    alert('error = ' + error);
+}
+function tokenHandler (result) {
+  // Your iOS push server needs to know the token before it can push to this device
+  // here is where you might want to send it the token for later use.
+  alert('device token = ' + result);
+}
+
+// DB on local mobile
 function initDb() {
   alert("INITDB");
   db = window.openDatabase("aftrworkDb", "1.0", "AftrWork DB", 1000000);
@@ -85,81 +134,82 @@ function initDb() {
 }
 function populateDB(tx) {
   alert("POPULATE");
-  tx.executeSql('DROP TABLE IF EXISTS OPTIONS');
-  tx.executeSql('CREATE TABLE IF NOT EXISTS OPTIONS (id unique, key, value)');
-  tx.executeSql('INSERT INTO OPTIONS (id, key, value) VALUES (1, "access_token", "hello")');
+  tx.executeSql("DROP TABLE IF EXISTS OPTIONS");
+  tx.executeSql("CREATE TABLE IF NOT EXISTS OPTIONS (id unique, key, value)");
+  tx.executeSql("INSERT INTO OPTIONS (id, key, value) VALUES (1, 'access_token', '" + access_token + "')" );
 }
-
 function queryDB(tx) {
   tx.executeSql('SELECT * FROM OPTIONS', [], querySuccess, errorCB);
 }
-
 // Query the success callback
 //
 function querySuccess(tx, res) {
-  var len = res.rows.length;
-  alert("OPTIONS table: " + len + " rows found.");
-  for (var i=0; i<len; i++){
-    alert("Row = " + i + " ID = " + res.rows.item(i).id + " Key =  " + res.rows.item(i).key + " Value = " + res.rows.item(i).value);
-  }
+  alert("token is " +  res.rows.item(0).value);
 }
-
+function querySuccessInit(tx, res) {
+  alert("token is " +  res.rows.item(0).value);
+  $.get("http://0.0.0.0:3000/hello").done(function(res) {
+    alert("OK");
+    alert(res);
+  }).fail(function(res) {
+    alert("NOK");
+    alert(res);
+  });
+}
 function errorCB(err) {
-  alert("Error processing SQL: "+err.code);
-}
 
+}
 function successCB() {
-  alert("success!");
-  db.transaction(queryDB, errorCB);
+
 }
 
-//Write and Read of a local file on mobile
-
-function initWrite() {
-  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFsWrite, fail);
-}
-function initRead() {
-  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFsRead, fail);
-}
-
-
-function gotFsWrite(fileSystem) {
-  fileSystem.root.getFile("localUserInfo", {create: true, exclusive: false}, gotFileEntryWrite, fail);
-}
-function gotFsRead(fileSystem) {
-  fileSystem.root.getFile("localUserInfo", {create: true, exclusive: false}, gotFileEntryRead, fail);
-}
-
-
-function gotFileEntryWrite(fileEntry) {
-  fileEntry.createWriter(gotFileWriter, fail);
-}
-
-function gotFileEntryRead(fileEntry) {
-  fileEntry.file(gotFileReader, fail);
-}
-
-
-function gotFileWriter(writer) {
-  writer.onwriteend = function(evt) {
-    // alert(access_token);
-  };
-  writer.write("Access Token: " + access_token );
-}
-
-function gotFileReader(file){
-  readAsText(file);
-}
-function readAsText(file) {
-  var reader = new FileReader();
-  reader.onloadend = function(evt) {
-    // alert(evt.target.result);
-  };
-  reader.readAsText(file);
-}
-
-
-function fail(error) {
-  alert("Error on saving your token localy");
-  // alert(error.code);
-}
+//
+// //Write and Read of a local file on mobile
+// function initWrite() {
+//   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFsWrite, fail);
+// }
+// function initRead() {
+//   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFsRead, fail);
+// }
+//
+//
+// function gotFsWrite(fileSystem) {
+//   fileSystem.root.getFile("localUserInfo", {create: true, exclusive: false}, gotFileEntryWrite, fail);
+// }
+// function gotFsRead(fileSystem) {
+//   fileSystem.root.getFile("localUserInfo", {create: true, exclusive: false}, gotFileEntryRead, fail);
+// }
+//
+//
+// function gotFileEntryWrite(fileEntry) {
+//   fileEntry.createWriter(gotFileWriter, fail);
+// }
+//
+// function gotFileEntryRead(fileEntry) {
+//   fileEntry.file(gotFileReader, fail);
+// }
+//
+//
+// function gotFileWriter(writer) {
+//   writer.onwriteend = function(evt) {
+//     // alert(access_token);
+//   };
+//   writer.write("Access Token: " + access_token );
+// }
+//
+// function gotFileReader(file){
+//   readAsText(file);
+// }
+// function readAsText(file) {
+//   var reader = new FileReader();
+//   reader.onloadend = function(evt) {
+//     // alert(evt.target.result);
+//   };
+//   reader.readAsText(file);
+// }
+//
+//
+// function fail(error) {
+//   alert("Error on saving your token localy");
+//   // alert(error.code);
+// }
